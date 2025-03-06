bbeb3bad635cd6f7e1d8ab053109c8e7a0d3af5b
ymoham55 <ymoham55@my.centennialcollege.ca>
2025-03-06 15:33:07 -0500

    Set up GitHub Actions workflow for testing, and added necessary references

--- SmartLocktests/UnitTest1.cs
+++ SmartLocktests/UnitTest1.cs
@@ -1,10 +1,56 @@
-namespace SmartLocktests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using MySmartLockNamespace; 
 
-public class UnitTest1
+namespace SmartLockTests
 {
-    [Fact]
-    public void Test1()
+    [TestClass]
+    public class UnitTest1
     {
+        
+        [TestMethod]
+        public void TestLockInitialization()
+        {
+            
+            var lockSystem = new SmartLock();  
 
+            
+            var result = lockSystem.IsLocked;
+
+           
+            Assert.IsFalse(result, "The lock should be initially unlocked.");
+        }
+
+        
+        [TestMethod]
+        public void TestLockSystem()
+        {
+          
+            var lockSystem = new SmartLock();  
+
+            
+            lockSystem.Lock();  
+            var result = lockSystem.IsLocked;
+
+            
+            Assert.IsTrue(result, "The lock should be locked after calling Lock().");
+        }
+
+        
+        [TestMethod]
+        public void TestUnlockSystem()
+        {
+            
+            var lockSystem = new SmartLock();  
+            lockSystem.Lock();  
+
+           
+            lockSystem.Unlock();  
+            var result = lockSystem.IsLocked;
+
+            
+            Assert.IsFalse(result, "The lock should be unlocked after calling Unlock().");
+        }
     }
 }
